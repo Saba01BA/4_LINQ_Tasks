9ef0a1846d4f452740103d6bb5c101d8bbe13fd8
saba sauri <sabasauri333@gmail.com>
2026-05-30 16:35:58 +0400

    Justa a Template, update is coming soon

---
+++ 4 LINQ Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4_LINQ_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
